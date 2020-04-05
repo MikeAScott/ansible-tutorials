14497456ba3f3de9e4f41eeff0a23b10f15b1edd
Mike Scott <MikeAScott@users.noreply.github.com>
2020-04-05 23:27:30 +0100

    Lab 02 (#1)
    * Refactor
    
    * Auto provision webserver
    
    Co-authored-by: Mike Scott <mike.scott@nttdata.com>

---
+++ scripts/install_ansible.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+ANSIBLE_USER=vagrant
+ANSIBLE_SSH_KEY=/home/$ANSIBLE_USER/.ssh/ansible_key
+
+apt-get update
+apt-get install -y ansible
+cp /vagrant/keys/ansible_key $ANSIBLE_SSH_KEY
+chown $ANSIBLE_USER:$ANSIBLE_USER $ANSIBLE_SSH_KEY
+chmod 400 $ANSIBLE_SSH_KEY
+touch /home/$ANSIBLE_USER/.ssh/known_hosts
+chown $ANSIBLE_USER:$ANSIBLE_USER /home/$ANSIBLE_USER/.ssh/known_hosts
+cp /vagrant/ansible_hosts /etc/ansible/hosts
